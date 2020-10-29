bcb58067832661f4788ff49fa340544d9dbf5d09
ocfemiajm <47093484+ocfemiajm@users.noreply.github.com>
2020-10-29 15:20:22 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,95 @@
+<footer class="bg-light text-dark pt-5 pb-4">
+        <div class="container text-center text-md-left">
+            <div class="row text-center text-md-left">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-info">About Us</h5>
+                    <hr class="mb-4">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga cupiditate consequatur, possimus culpa modi numquam quasi veritatis nihil accusamus. Tempore suscipit blanditiis beatae eveniet nam adipisci expedita illo necessitatibus ullam!</p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-info">Let Us Help</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Your Account</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Your Orders</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Manage Your Content And Devices</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Help</a>
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-info">Make Money</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Sell Products on Our Website</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Advertise Your Products</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Become An Affiliate</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-dark" style="text-decoration:none">Self-Publish</a>
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-info">Contact Us</h5>
+                    <hr class="mb-4">
+                    <p>
+                        <li class="fas fa-home mr-3"></li>Unit 21 Technopark Square Laguna Technopark Gate 3 Barangay Malamig Biñan Laguna Philippines 4024
+                    </p>
+                    <p>
+                        <li class="fas fa-envelope mr-3"></li>hello@safetywellmarketing.net
+                    </p>
+                    <p>
+                        <li class="fas fa-phone mr-3"></li>02 959 5255 - 049 559 6345- 0917 888 9022 - 09338161098
+                    </p>
+                    <!-- <p>
+                        <li class="fas fa-print mr-3"></li>+63944841715
+                    </p> -->
+                </div>
+            </div>
+                <hr class="mb-4">
+
+                <div class="row d-flex justify-content-center">
+                    <div>
+                        <p>
+                            © Copyright 2020 All Rights Reserved By: 
+                            <a href="#" style="text-decoration: none;">
+                                <strong class="text-info"> Safetywell Marketing Incorporated</strong>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row d-flex justify-content-center">
+                    <div class="text-center">
+                        <ul class="list-unstyled list-inline">
+                            <li class="list-inline-item">
+                                <a href="#" class="text-dark"><i class="fab fa-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="text-dark"><i class="fab fa-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="text-dark"><i class="fab fa-google-plus"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://www.youtube.com/watch?v=wjCvHzwRkcE&feature=youtu.be" class="text-dark"><i class="fab fa-youtube"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
